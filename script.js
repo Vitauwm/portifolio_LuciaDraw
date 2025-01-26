3ab730b88072139fad76ef04c397c32504603090
Vitauwm <vyctorbruno892@gmail.com>
2025-01-26 15:17:09 -0300

    teste

--- script.js
+++ script.js
@@ -5,16 +5,3 @@ function openModal(modalId) {
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
-// Função para abrir o modal e exibir a imagem clicada
-function openModal(src) {
-    var modal = document.getElementById("myModal");
-    var modalImg = document.getElementById("img01");
-    modal.style.display = "block";
-    modalImg.src = src;
-}
-
-// Função para fechar o modal
-function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-}
